f2fbeff535e0536890f0fb6c72f9f09efd9226bf
sergioadll <sergio.adll@hotmail.com>
2020-08-29 15:57:09 +0000

    basic-structure-home-login

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,97 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+	const [showMenu, setShowMenu] = useState(false);
+	const [dropMenu, setDropMenu] = useState("collapse bg-dark");
+
+	const changeMenu = () => {
+		setShowMenu(showMenu => !showMenu);
+		if (showMenu == true) {
+			setDropMenu(dropMenu => "bg-dark");
+		} else {
+			setDropMenu(dropMenu => "collapse bg-dark");
+		}
+	};
+
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<header>
+			<div className={dropMenu} id="navbarHeader">
+				<div className="container">
+					<div className="row">
+						<div className="col-sm-8 col-md-7 py-4">
+							<h4 className="text-white">About</h4>
+							<p className="text-muted">
+								Add some information about the album below, the author, or any other background context.
+								Make it a few sentences long so folks can pick up some informative tidbits. Then, link
+								them off to some social networking sites or contact information.
+							</p>
+						</div>
+						<div className="col-sm-4 offset-md-1 py-4">
+							<h4 className="text-white">Contact</h4>
+							<ul className="list-unstyled">
+								<li>
+									<a href="#" className="text-white">
+										Follow on Twitter
+									</a>
+								</li>
+								<li>
+									<a href="#" className="text-white">
+										Like on Facebook
+									</a>
+								</li>
+								<li>
+									<a href="#" className="text-white">
+										Email me
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
 			</div>
-		</nav>
+			<div className="navbar navbar-dark bg-dark shadow-sm">
+				<div className="container d-flex justify-content-between">
+					<a href="/" className="navbar-brand d-flex align-items-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							fill="none"
+							stroke="currentColor"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							aria-hidden="true"
+							className="mr-2"
+							viewBox="0 0 24 24"
+							focusable="false">
+							<path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
+							<circle cx="12" cy="13" r="4" />
+						</svg>
+						<strong>finMATH</strong>
+					</a>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-toggle="collapse"
+						data-target="#navbarHeader"
+						aria-controls="navbarHeader"
+						aria-expanded={showMenu}
+						aria-label="Toggle navigation">
+						<span
+							className="navbar-toggler-icon"
+							onClick={() => {
+								changeMenu();
+							}}
+						/>
+					</button>
+
+					<a href="/login" className="btn btn-success">
+						Success
+					</a>
+				</div>
+			</div>
+		</header>
 	);
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,25 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Card } from "../component/card.js";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	return (
+		<section className="">
+			<form className="form-inline my-4 justify-content-center">
+				<input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+				<button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+					Search
+				</button>
+			</form>
+			<div className="card-group">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
+		</section>
+	);
+};
